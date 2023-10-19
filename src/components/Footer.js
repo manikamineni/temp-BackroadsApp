4cf5bfcd8c61108edaf7b6c5824f381b0a9e4f66
manikamineni <94723402+manikamineni@users.noreply.github.com>
2023-10-19 18:53:31 -0400

    first-commit

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {pageLinks,socialLinks} from '../data'
+
+const Footer = () => {
+  return (
+    <footer className='section footer'>
+      <ul className='footer-links'>
+        {pageLinks.map((link) => (
+          <li key={link.id}>
+            <a href={link.href} className='footer-link'>
+              {link.text}
+            </a>
+          </li>
+        ))}
+        <li>
+          <a href='#featured' className='footer-link'>
+            featured
+          </a>
+        </li>
+      </ul>
+      <ul className='footer-icons'>
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target='_blank'
+                rel='noreferrer'
+                className='footer-icon'
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className='copyright'>
+        copyright &copy; Backroads travel tours company
+        <span id='date'>{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
